1b7827de104c87d99bef7840f2af4ce20e71b94c
Marcos <mvra0802@gmail.com>
2026-09-16 16:14:01 -0300

    commit inicial do projeto

---
+++ src/main/java/com/marcos/operadora_cartao_credito/OperadoraCartaoCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.operadora_cartao_credito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperadoraCartaoCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperadoraCartaoCreditoApplication.class, args);
+	}
+
+}
